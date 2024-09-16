6c5c74de36f95114252df4dc498b1be75191e17d
getutensin <getutensin@gmail.com>
2024-09-16 18:50:26 +0900

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        // 現在の時刻を取得
+        LocalTime currentTime = LocalTime.now();
+
+        // 時刻を表示
+        System.out.println("現在の時刻: " + currentTime);
+    }
+}
